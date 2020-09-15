183c63da510d35f5fddf023be4c384347277cd7f
venki <venkihebbarkuru@gmail.com>
2020-09-15 16:21:06 +0530

    new file comment

---
+++ src/test/java/selng/newselngproject/AppTest.java
@@ -0,0 +1,41 @@
+package selng.newselngproject;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue(){
+    {
+    	
+    	System.out.println("Test 1");
+    }
+    
+    }
+    
+    
+    @Test
+    public void shouldAnswerWithTrue2(){
+    {
+    	
+    	System.out.println("Test 2");
+    }
+    
+    }
+    
+    
+    @Test
+    public void shouldAnswerWithTrue3(){
+    {
+    	
+    	System.out.println("Test 3");
+    }
+    
+    }
+}
